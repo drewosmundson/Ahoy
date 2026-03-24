dc696a87a274b0c090f5c9fb07b3aa115bbb4795
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-03-24 02:06:56 -0500

    Update SoundControllers.js
    Added method to attach camera to sound controller class

--- src/client/game/utils/SoundControllers.js
+++ src/client/game/utils/SoundControllers.js
@@ -8,22 +8,41 @@
 
 
 class SoundController{
+  
+  #defaults = { 
+    
+    
+    } 
 
   constructor(camera) {
-    this.camera = camera
     this.listener = new THREE.AudioListener();
-
-    this.masterVolume = 1.0;
-    this.musicVolume = 0.5;
-    this.sfxVolume = 0.7;
+    this.currentCamera = null;
+    this.attachCamera(camera)
+    
+    this.masterVolume = this.#defaults.masterVolume;
+    this.musicVolume = this.#defaults.musicVolume;
+    this.sfxVolume = this.#defaults.sfxVolume;
 
   }
-  loadSoundEffect(){
+  attachCamera(camera){
+   if(this.currentCamera){
+      this.listener.remove(this.camera);
+    }
+    this.listener.add(camera);
+  }
+    
+  
+  resetToDefaults() {
+    this.masterVolume = this.#defaults.masterVolume;
+    this.musicVolume = this.#defaults.musicVolume;
+    this.sfxVolume = this.#defaults.sfxVolume;
+  }
 
+  
+  loadSoundEffect(){
     this.soundManager = new SoundManager(this.camera);
     this.soundManager.loadSoundEffect('mainTheme', 'resources/sounds/Lost_Sheep_Compressed.mp3');
     this.soundManager.loadSoundEffect('ambient', 'resources/sounds/waves.mp3');
     this.playBackgroundMusic();
-
   }
 }
