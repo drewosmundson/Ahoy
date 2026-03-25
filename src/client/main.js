3e83771daf1a9f4afd229681fb589df193ca55d9
Drew Osmundson <drewosmundson@gmail.com>
2026-03-24 22:07:31 -0500

    file organize

--- src/client/main.js
+++ src/client/main.js
@@ -20,13 +20,14 @@
 import { initDom } from "./app/dom.js";
 import { initNavigation } from "./app/navigation.js";
 import { initSingleplayer } from "./game/singleplayer.js";
-import { initLobby } from "./features/Lobby.js";
-import { initMMO } from "./features/MMO.js";
+import { initLobby } from "./game/Lobby.js";
+import { initMMO } from "./game/MMO.js";
 
-// Eventually as this list becomes large or I am looking to break up this file
+// Eventually as this list becomes large or I am looking to break up this file    
 // one idea I have is to create a features folder in this layer that will contain
 // singleplayer.js mmo.js multiplayer.js so ownership of those features become more 
-// independant. This would be a page/feature module archetecture.
+// independant. This would be a page/feature module archetecture.   
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
