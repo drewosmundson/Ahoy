ecfedf2301acfb488d867d54a2c373973d0f0998
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-04-16 23:51:18 -0500

    Update singleplayer.js

--- src/client/features/singleplayer.js
+++ src/client/features/singleplayer.js
@@ -3,12 +3,18 @@
 
 
 
+
 export function createSingleplayer({ dom, navigate, startGame }) {
   return {
-    initialize
+    initialize,
+    start
   };
 
   function initialize() {
+    dom.buttons.mainToSingleplayer?.addEventListener('click', () => {
+      navigate.toScreen(dom.screens.singleplayer);
+    });
+
     dom.buttons.singleplayerStart?.addEventListener('click', start);
   }
 
