eb7f968dd71ae32679be72d6493baef90b5eae57
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-04-20 23:45:52 -0500

    Update navigation.js no longer imports dom. Instead handles via dependency injection.

--- src/client/app/navigation.js
+++ src/client/app/navigation.js
@@ -1,59 +1,30 @@
-import { CSS_HIDDEN_CLASS } from "./constants.js";
-import { dom } from "./dom.js";
+import { CSS_HIDDEN } from "./constants.js";
 
 
-export function nav(dom) { 
+export function navigation(dom) { 
   
   return { initalize, toScreen };
   
-  function initalize() { 
-  window.addEventListener("popstate", (event) => {
-    const screenId = event.state?.screen;
-    const screen = screenId ? document.getElementById(screenId) : dom.screens.mainMenu;
-    if (screen) navigateToScreen(screen);
-  });
-  navigation.toScreen(dom.screens.mainMenu);
-}
-
-function toScreen(screen) {
-  hideAllScreens(dom.screens);
-  showScreen(screen);
-  updateTitle(screen)
-  updateNavigationHistory(screen);
-}
-  
-  
-  } 
-
-
-
-
-export const navigation = {
-  initalize,
-  toScreen
-};
-
-// Allows for the forward and back buttons in the browser
+  // Allows for the forward and back buttons in the browser
 // always puts the user back to the home screen if they leave the site and come back they can rejoin their lobby from there if they were in one
-function initalize() { 
-  window.addEventListener("popstate", (event) => {
-    const screenId = event.state?.screen;
-    const screen = screenId ? document.getElementById(screenId) : dom.screens.mainMenu;
-    if (screen) navigateToScreen(screen);
-  });
-  navigation.toScreen(dom.screens.mainMenu);
+  function initalize() { 
+    window.addEventListener("popstate", (event) => {
+      const screenId = event.state?.screen;
+      const screen = screenId ? document.getElementById(screenId) : dom.screens.mainMenu;
+      if (screen) navigateToScreen(screen);
+      });
+    navigation.toScreen(dom.screens.mainMenu);
+  }
+
+  function toScreen(screen) {
+    hideAllScreens(dom);
+    showScreen(screen);
+    updateTitle(screen)
+    updateNavigationHistory(screen);
+  }
 }
-
-function toScreen(screen) {
-  hideAllScreens(dom.screens);
-  showScreen(screen);
-  updateTitle(screen)
-  updateNavigationHistory(screen);
-}
-
-
-function hideAllScreens(allScreens){
-  Object.values(allScreens).forEach((screen) => {
+function hideAllScreens(dom){
+  Object.values(dom.screens).forEach((screen) => {
     if (screen) { 
       screen.classList.add(CSS_HIDDEN);
     }
@@ -64,7 +35,7 @@ function hideAllScreens(allScreens){
 }
 
 function showScreen(screen) {
-  screen.classList.remove(CSS_HIDDEN_CLASS);
+  screen.classList.remove(CSS_HIDDEN);
   if ( screen === dom.screens.game ) {
     dom.canvas.game.style.display = 'block'
   }
