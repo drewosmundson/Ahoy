c838507020b73056111125002645ce2ef5d8bf59
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-04-21 12:58:00 -0500

    Update ui.js to have dependency injection for dom

--- src/client/app/ui.js
+++ src/client/app/ui.js
@@ -2,22 +2,26 @@
 import { dom } from "./dom.js";
 
 
-// functions that update the DOM 
-export function updatePlayerList(players) {
-  dom.playerListItems.forEach(el => {
-    if (!el) return;
-    el.innerHTML = '';
-    players.forEach(player => {
-      const li = document.createElement('li');
-      li.textContent = player.name || player.id;
-      if (player.isHost) li.textContent += ' (Host)';
-      el.appendChild(li);
-    });
-  });
-}
+export function createUI(dom) { 
+  return {updatePlayerList, updateLobbyCodeDisplay}
 
-export function updateLobbyCodeDisplay(lobbyId) {
-  dom.lobbyCodeDisplay.forEach(el => {
-    if (el) el.textContent = lobbyId;
-  });
+  // functions that update the DOM 
+  function updatePlayerList(players) {
+    dom.playerListItems.forEach(el => {
+      if (!el) return;
+      el.innerHTML = '';
+      players.forEach(player => {
+        const li = document.createElement('li');
+        li.textContent = player.name || player.id;
+        if (player.isHost) li.textContent += ' (Host)';
+        el.appendChild(li);
+      });
+    });
+  }
+  
+  function updateLobbyCodeDisplay(lobbyId) {
+    dom.lobbyCodeDisplay.forEach(el => {
+      if (el) el.textContent = lobbyId;
+    });
+    }
 }
