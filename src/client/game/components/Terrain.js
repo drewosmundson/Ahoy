da33d034be1fea5cc5ac2e12be047a01f2787988
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-04-26 00:22:26 -0500

    Update Terrain.js

--- src/client/game/components/Terrain.js
+++ src/client/game/components/Terrain.js
@@ -1,3 +1,45 @@
 // recveives terrain data from the server and displays this terrain to the user with 3js
 
 
+
+terrain renderer() {
+    
+    
+    }
+
+
+
+
+import constants 
+
+
+export class terrain () { 
+
+
+    generator() { 
+        
+        
+        } 
+
+)
+
+
+} 
+
+class Heightmap () { 
+
+generator() {}
+
+} 
+
+class Noise() { 
+
+
+generator(){ 
+
+} 
+
+
+class Simplex() {
+
+} 
