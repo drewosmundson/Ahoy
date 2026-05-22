b1e0b6d536b362fb4fd3ce064a19f03f206a2083
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-05-22 00:30:24 -0500

    Organizing main. Possible Create new instance of game from here so many game objects are not being made constantly.

--- src/client/main.js
+++ src/client/main.js
@@ -1,7 +1,4 @@
 
-
-
-
 // grab DOM
 // wire modules together
 // start navigation
@@ -13,7 +10,6 @@ import { createDom } from "./app/dom.js";
 import { createUi } from "./app/ui.js"
 import { createEmitter } from "./app/emitter.js"
 
-
 // page/feature module archetecture. 
 import { singleplayer } from "./features/singleplayer.js"
 import { host } from "./features/host.js"
@@ -21,21 +17,26 @@ import { participant } from "./features/participant.js";
 import { mmo } from "./features/mmo.js"
 
 import { eventSchemas } from "../shared/schemas.js";
-import { CONSTANTS } from "../shared/CONSTANTS.js"
 
 import { Game } from "./game/Game.js";
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  const socket = io();
   const dom = createDom();
-
+  const navigate = createNavigation(dom);
+  const ui = createUi(dom);
+  
+  const socket = io();
+  const eventSchemas = createEventSchemas();
+  const emitter =  createEmitter(socket, eventSchemas);
+  
+  //const game = new Game();
+  
   const context = {
     dom,
+    navigate,
+    ui,
+    emitter,
     Game,
-    emitter: createEmitter(socket, eventSchemas),
-    navigate: createNavigation(dom),
-    ui: createUi(dom),
   };
   // As this becomes large it would be good practice to inialize only the nessesary event listeners
   // For now this is fine as there are only about 3 event listeners for each feature
