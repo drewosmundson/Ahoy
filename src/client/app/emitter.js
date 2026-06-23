901795066220b97070ba1e75c3238d3cd338ff9c
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-06-23 01:18:21 -0500

    Update emitter.js

--- src/client/app/emitter.js
+++ src/client/app/emitter.js
@@ -1,32 +1,56 @@
 
-
-export function createEmitter(socket, eventSchemas)  {
-  return {
-    emit,
-    nullEmit
-  }
-  
-  function emit(event, data, lobby) {
-    if (!(event in eventSchemas)) {
-        throw new Error(`Unknown event: ${event}`);
+// event handler does not know about sockets
+export class EventHandler {
+    constructor() {
+        this.listeners = new Map();
     }
 
-    const isValid = eventSchemas[event](data);
-
-    if (!isValid) {
-        throw new Error(`Invalid payload for ${event}`);
+    on(event, callback) {
+        if (!this.listeners.has(event)) {
+            this.listeners.set(event, []);
+        }
+        this.listeners.get(event).push(callback);
+        return {
+            unsubscribe: () => {
+                this.off(event, callback);
+            },
+            // pause: () => {} 
+        };
     }
-    socket.emit(event, {...data, lobby});
-  }
-    // Intentionally does nothing. 
-  // The purpose of this function is to be passed into a singleplayer games
-  // That way no logic needs to change and no additional data has to be attempted 
-  // to be passed to the server. 
-    function nullEmit() {}
+
+    off(event, callback) {
+        const callbacks = this.listeners.get(event);
+        
+        if (!callbacks) return;
+        
+        const index = callbacks.indexOf(callback);
+        
+        if (index !== -1) {
+            callbacks.splice(index, 1);
+        }
+        if (callbacks.length === 0) {
+            this.listeners.delete(event);
+        }
+    }
+
+    emit(event, data) {
+        const callbacks = this.listeners.get(event);
+        if (!callbacks) return;
+        for (const callback of callbacks) {
+            callback(data);
+        }
+    }
+
+    attach(subscribe) {
+        return subscribe((event, data) => {
+            this.emit(event, data);
+        });
+    }
 }
 
-export function createEmitter(socket, eventSchemas) {
-    return function emit(event, data, lobby) {
+
+export function createSender(socket, eventSchemas) {
+    return function send(event, data, lobby) {
         if (!(event in eventSchemas)) {
             throw new Error(`Unknown event: ${event}`);
         }
@@ -35,24 +59,97 @@ export function createEmitter(socket, eventSchemas) {
             throw new Error(`Invalid payload`);
         }
 
-        socket.emit(event, {
-            ...data,
-            lobby
-        });
+        socket.emit(event, { ...data, lobby });
     };
 }
+
 
 export function createReceiver(socket, eventSchemas) {
     return function subscribe(handler) {
-        for (const event in eventSchemas) {
-            socket.on(event, data => {
-                if (!eventSchemas[event](data)) {
-                    return;
-                }
+        const cleanup = [];
 
+        for (const event in eventSchemas) {
+            const listener = data => {
+                if (!eventSchemas[event](data)) return;
+              
                 handler(event, data);
+            };
+
+            socket.on(event, listener);
+
+            cleanup.push(() => {
+                socket.off(event, listener);
             });
         }
+
+        return {
+            unsubscribe() {
+                cleanup.forEach(fn => fn());
+            }
+        };
     };
 }
 
+
+export class NetworkInterface {
+    constructor(socket, eventSchemas) {
+        this.events = new EventHandler();
+        this.send = createSender(socket, eventSchemas);
+        this.recive  = createReceiver(socket, eventSchemas);
+        this.subscription = this.events.attach(recive);
+    }
+
+    on(event, callback) {
+        return this.events.on(event, callback);
+    }
+
+    sendEvent(event, data, lobby) {
+        this.send(event, data, lobby);
+    }
+
+    disconnect() {
+        this.subscription?.unsubscribe();
+    }
+}
+
+
+/*
+// ----------------------
+// Usage
+// ----------------------
+
+const network = new NetworkInterface(socket, eventSchemas);
+
+
+// Receive network events
+const sub = network.on(
+        "playerJoined",
+      data => {
+            console.log(
+                "Player joined:",
+                data
+            );
+        }
+    );
+
+
+// Send network events
+network.sendEvent(
+    "move",
+    {
+        x: 10,
+        y: 20
+    },
+    "room-1"
+);
+
+
+// Stop listening locally
+sub.unsubscribe();
+
+
+// Stop receiving socket events
+network.disconnect();
+
+
+*/
