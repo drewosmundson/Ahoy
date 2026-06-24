76dd571d65beaef4c242cb218b488a6a2abb4631
Drew Osmundson <drewosmundson@gmail.com>
2026-06-23 23:15:56 -0500

    refactor(app): update event handling and network interface

--- src/client/app/emitter.js
+++ src/client/app/emitter.js
@@ -1,57 +1,8 @@
 
-// event handler does not know about sockets
-// can be used locally for ascync updates like user input 
-export class EventHandler {
-    constructor() {
-        this.listeners = new Map();
-    }
-
-    on(event, callback) {
-        if (!this.listeners.has(event)) {
-            this.listeners.set(event, []);
-        }
-        this.listeners.get(event).push(callback);
-        return {
-            unsubscribe: () => {
-                this.off(event, callback);
-            },
-            // pause: () => {} 
-        };
-    }
-
-    off(event, callback) {
-        const callbacks = this.listeners.get(event);
-        
-        if (!callbacks) return;
-        
-        const index = callbacks.indexOf(callback);
-        
-        if (index !== -1) {
-            callbacks.splice(index, 1);
-        }
-        if (callbacks.length === 0) {
-            this.listeners.delete(event);
-        }
-    }
-
-    emit(event, data) {
-        const callbacks = this.listeners.get(event);
-        if (!callbacks) return;
-        for (const callback of callbacks) {
-            callback(data);
-        }
-    }
-
-    attach(subscribe) {
-        return subscribe((event, data) => {
-            this.emit(event, data);
-        });
-    }
-}
 
 
-export function createSender(socket, eventSchemas) {
-    return function send(event, data, lobby) {
+function createSender(socket, eventSchemas) {
+    return function send(event, data) {
         if (!(event in eventSchemas)) {
             throw new Error(`Unknown event: ${event}`);
         }
@@ -60,12 +11,12 @@ export function createSender(socket, eventSchemas) {
             throw new Error(`Invalid payload`);
         }
 
-        socket.emit(event, { ...data, lobby });
+        socket.emit(event, { ...data });
     };
 }
 
 
-export function createReceiver(socket, eventSchemas) {
+function createReceiver(socket, eventSchemas) {
     return function subscribe(handler) {
         const cleanup = [];
 
@@ -85,27 +36,84 @@ export function createReceiver(socket, eventSchemas) {
 
         return {
             unsubscribe() {
-                cleanup.forEach(fn => fn());
+                cleanup.forEach(func => func());
             }
         };
     };
 }
 
 
+
+
+// event handler does not know about sockets
+// can be used locally for ascync updates like user input 
+export class EventHandler {
+    constructor() {
+        this.listeners = new Map();
+    }
+
+    on(event, callback) {
+        if (!this.listeners.has(event)) {
+            this.listeners.set(event, new Set());
+        }
+        this.listeners.get(event).add(callback)
+        return {
+            unsubscribe: () => {
+                this.off(event, callback);
+            },
+            // pause: () => {} 
+        };
+    }
+
+    off(event, callback) {
+        const callbacks = this.listeners.get(event);
+        
+        if (!callbacks) return;
+        
+        callbacks.delete(callback);
+
+        if (callbacks.size === 0) {
+            this.listeners.delete(event);
+        }
+    }
+
+    emit(event, data) {
+        const callbacks = this.listeners.get(event);
+        if (!callbacks) return;
+        for (const callback of [...callbacks]) {
+            try {
+                callback(data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+
+    attach(subscribe) {
+        return subscribe((event, data) => {
+            this.emit(event, data);
+        });
+    }
+}
+
+
+
+
+
 export class NetworkInterface {
     constructor(socket, eventSchemas) {
         this.events = new EventHandler();
         this.send = createSender(socket, eventSchemas);
-        this.recive  = createReceiver(socket, eventSchemas);
-        this.subscription = this.events.attach(recive);
+        this.receive = createReceiver(socket, eventSchemas);
+        this.subscription = this.events.attach(this.receive);
     }
 
     on(event, callback) {
         return this.events.on(event, callback);
     }
 
-    sendEvent(event, data, lobby) {
-        this.send(event, data, lobby);
+    sendEvent(event, data) {
+        this.send(event, data);
     }
 
     disconnect() {
@@ -115,9 +123,7 @@ export class NetworkInterface {
 
 
 /*
-// Usage
-
-// Local 
+---- Local Events----
 const events = new EventHandler();
 
 events.on("modalOpened", data => {
@@ -128,15 +134,10 @@ events.on("scoreChanged", score => {
     console.log("Score:", score);
 });
 
-events.emit(
-    "modalOpened",
-    { id: "settings" }
-);
+events.emit("modalOpened", { id: "settings" });
 
-events.emit(
-    "scoreChanged",
-    100
-);
+events.emit("scoreChanged", 100);
+
 class Player {
     constructor(events) {
         this.events = events;
@@ -145,44 +146,31 @@ class Player {
 
     damage(amount) {
         this.health -= amount;
-        this.events.emit("playerDamaged", { health: this.health }
-        );
+        this.events.emit("playerDamaged", { health: this.health });
     }
 }
 
-
-
-//Network 
+----- Network Events ----- 
 const network = new NetworkInterface(socket, eventSchemas);
 
-// Receive network events
-const sub = network.on(
-        "playerJoined",
-      data => {
-            console.log(
-                "Player joined:",
-                data
-            );
-        }
-    );
+Receive network events
+const sub = network.on("playerJoined", data => {
+    console.log("Player joined:", data);
+});
 
 
-// Send network events
-network.sendEvent(
-    "move",
-    {
-        x: 10,
-        y: 20
-    },
-    "room-1"
-);
+Send network events
+network.sendEvent("move", {
+    x: 10,
+    y: 20
+});
 
 
-// Stop listening locally
+Stop listening locally
 sub.unsubscribe();
 
 
-// Stop receiving socket events
+Stop receiving socket events
 network.disconnect();
 
 
