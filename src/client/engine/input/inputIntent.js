3ed02e49408eeba74ac48132c43fa001c50fe31d
Drew Osmundson <drewosmundson@gmail.com>
2026-07-27 21:58:19 -0500

    refactor(input): update input handling and event listeners

--- src/client/engine/input/inputIntent.js
+++ src/client/engine/input/inputIntent.js
@@ -1,26 +1,11 @@
-class ClientInput {
-    constructor(events) {
-        this.events = events;
 
-        document.addEventListener('keydown', (event) => {
-            this.handleKeyDown(event);
-        });
-        document.addEventListener('keyup', (event) => {
-            this.handleKeyUp(event);
-            this.update();
-        });
-        document.addEventListener('mousedown', (event) => {
-            this.handleKeyDown(event);
-            this.update();
-        });
-        document.addEventListener('mouseup', (event) => {
-            this.handleKeyUp(event);
-            this.update();
-        })
-        document.addEventListener('mousemove', (event) => {
-            this.handleMouseMove(event);
-            this.updateMouse()
-        });
+
+
+
+
+
+class ClientInput {
+    constructor(eventBus) {
 
         this.keyBindings = {
             0:          'fireProjectileLeft',
@@ -34,100 +19,89 @@ class ClientInput {
             KeyD:       'moveRight',
             ArrowRight: 'moveRight',
             KeyM:       'showMap',
-            KeyC: 'toggleCamera',
-            KeyP: 'toggleTerrain',
-            KeyF: 'toggleFog',
-            Escape: 'exitPointerLock',
+            KeyC:       'toggleCamera',
+            KeyP:       'toggleTerrain',
+            KeyF:       'toggleFog',
+            Escape:     'exitPointerLock',
         };
-        // make this into a bitmask to compare against what changed then emit what changed
-        // example last emit
-        //wsad LR   pctf xy
-        //0000 0000 0000 0000
-        //0000 0100 1000 0000
 
-        this.actions = {
-            moveForward:  false,
-            moveBackward: false,
-            moveLeft:     false,
-            moveRight:    false,
-            fireProjectileLeft:  false,
-            fireProjectileRight: false,
+        this.mouseMovementData = {
+            timestamp: 0,
+            dx: 0,
+            dy: 0,
+        };
+
+        this.actionData = {
+            timestamp: 0,
+            action: 0,
         }
 
-        this.toggles = {
-            pointerLocked: false,
-            toggleCamera:  false,
-            toggleTerrain: false,
-            toggleFog:     false,
-        }
+        // Keydown Event
+        const downActionHandler = (event) => {
+            const data = this.handleActionEvent(event);
+            if (!data) return;
+            eventBus.emit("buttonPressed", data);
+        };
 
-        this.mouseMovement = {
-            deltaPitch: 0, 
-            deltaYaw:   0,
-        }
+        ["keydown", "mousedown"].forEach(type => {
+            document.addEventListener(type, downActionHandler);
+        });
+
+
+        // Keyup Event 
+        const upActionHandler = (event) => {
+            const data = this.handleActionEvent(event);
+            if (!data) return;
+            eventBus.emit("buttonReleased", data);
+        };
+        ["keyup", "mouseup"].forEach(type => {
+            document.addEventListener(type, upActionHandler);
+        });
+
+
+        // Mouse Movement Event
+        const movementActionHandler = (event) => {
+            const data = this.handleMovementEvent(event);
+            if (!data) return;
+            eventBus.emit("mouseMovement", data);
+        };
+        ["mousemove"].forEach(type => {
+            document.addEventListener(type, movementActionHandler);
+        });
     }
 
-    handleKeyDown(event) {
+    handleActionEvent(event) {
         const eventCode = event.code;
         const eventButton = event.button
-        const buttonPressed = this.keyBindings[eventCode] ?? this.keyBindings[eventButton];
-        if (!buttonPressed) return;
-        if (!event.repeat && this.actions[buttonPressed] !== undefined) {
-            const data = {
-                this.actions[buttonPressed],
-                true
-            }
-            this.events.emit("actionButtonPressed", data)
-        }
-        if (!event.repeat && this.toggles[buttonPressed] !== undefined) {
-            this.toggles[buttonPressed] = !this.toggles[buttonPressed];
-        }
+        const action = this.keyBindings[eventCode] ?? this.keyBindings[eventButton];
+        if (event.repeat) return;
+        if (!action) return;
+        const data = this.actionData;
+        data.timestamp = performance.now(),
+        data.action = action;
+        return data 
     }
 
-    handleKeyUp(event) {
-        const eventCode = event.code;
-        const eventButton = event.button;
-        const buttonReleased = this.keyBindings[eventCode] ?? this.keyBindings[eventButton];
-        if (!buttonReleased) return;
-        if ( this.actions[buttonReleased] !== undefined) {
-            this.actions[buttonReleased] = false;
-        }
+    handleMovementEvent(event) {
+        const data = this.mouseMovementData;
+        data.timestamp = performance.now();
+        data.dx = event.movementX;
+        data.dy = event.movementY;
+        return data;
     }
 
-    handleMouseMove(event) {
-        if (!this.toggles.pointerLocked) return;
-        this.mouseMovement.deltaPitch += event.movementY;
-        this.mouseMovement.deltaYaw   += event.movementX;
+
+    removeEventListeners() {
+        ["keydown", "mousedown"].forEach(type => {
+            document.removeEventListener(type, downActionHandler);
+        });
+
+        ["keyup", "mouseup"].forEach(type => {
+            document.removeEventListener(type, upActionHandler);
+        });
+
+        ["mousemove"].forEach(type => {
+            document.removeEventListener(type, movementActionHandler);
+        });
     }
-
-    // Actions that need to be reset to their default go here
-    resetOneTimeActions() {
-
-    }
-    // It is very unlikely that this will ever be needed as event.movementX resets to 0 after each event and update() is called after each event
-    // this function exists for safty and asurance that the same movement of the mouse will always be deterministic
-    resetMouse() {
-        this.mouseMovement.deltaPitch = 0;
-        this.mouseMovement.deltaYaw = 0;
-        // this.mouseMovement.mousewheel = 0;
-    }
-
-    getSnapshot() {
-        const snapshot = {
-            timestamp: performance.now(),
-            actions: { ...this.actions },
-            toggles: { ...this.toggles },
-        }
-        return snapshot
-    }
-
-    update() { 
-        const mouseData = this.mouseMovement
-        this.events.emit("mouseMove", mouseData);
-        this.resetMouse() 
-
-        const snapshot = this.getSnapshot()
-        this.events.emit("snapshot", snapshot);
-        this.resetOneTimeActions() 
-    } 
 }
