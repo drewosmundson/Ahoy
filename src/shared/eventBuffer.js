31321bba08f921002bc117624e9407172add29c8
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-08-18 22:45:37 -0500

    Update eventBuffer.js

--- src/shared/eventBuffer.js
+++ src/shared/eventBuffer.js
@@ -75,16 +75,8 @@ class EventBuffer {
     
     
     // to test each of the 3 methods 
-testing() 
-    test(0) 
-    test(1) 
-    test(2) 
-    test(10)
-    test(20)
-    test(100) 
-    test(1000) 
-    
-    function test(listLength) {
+testing() {
+    const test = (listLength) => {
         const iterations = 10000
         const comparingFunctions = [
             this.drain,
@@ -119,8 +111,178 @@ testing()
             console.log ("tested Function: ${func}
              comsole.log("time taken:" ${averageTime} 
              console.log(`Used Heap: ${averageMemory} / 1024 / 1024} MB`);
-        } 
-    } 
+        }
+    }
+    test(0) 
+    test(1) 
+    test(2) 
+    test(10)
+    test(20)
+    test(100) 
+    test(1000) 
+    
 }
+
+class Bus {
+    on(){}
+}
+
+const bus = new Bus()
+const eventBuffer = new EventBuffer(bus)
+eventBuffer.testing() 
+
+
+class EventBuffer {
+    constructor(eventBus, bufferedEvent) {
+        this.queue = [];
+
+        eventBus.on(bufferedEvent, (data) => {
+            this.queue.push(data);
+        });
+    }
+
+    drain() {
+        const items = this.queue;
+        this.queue = [];
+        return items;
+    }
+
+    drainSet() {
+        const items = [...new Set(this.queue)];
+        this.queue = [];
+        return items;
+    }
+
+    drainObject() {
+        const items = [];
+        const seen = Object.create(null);
+
+        for (const item of this.queue) {
+            const key = String(item);
+
+            if (!seen[key]) {
+                seen[key] = true;
+                items.push(item);
+            }
+        }
+
+        this.queue = [];
+        return items;
+    }
+
+    drainIndexOf() {
+        const items = [];
+
+        for (const item of this.queue) {
+            if (items.indexOf(item) === -1) {
+                items.push(item);
+            }
+        }
+
+        this.queue = [];
+        return items;
+    }
+
+    drainManual() {
+        const items = [];
+
+        for (const item of this.queue) {
+            let duplicate = false;
+
+            for (let i = 0; i < items.length; i++) {
+                if (items[i] === item) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (!duplicate) {
+                items.push(item);
+            }
+        }
+
+        this.queue = [];
+        return items;
+    }
+
+    drainSmall() {
+        const queue = this.queue;
+        this.queue = [];
+
+        if (queue.length <= 1) {
+            return queue;
+        }
+
+        if (queue.length === 2) {
+            return queue[0] === queue[1]
+                ? [queue[0]]
+                : queue;
+        }
+
+        return [...new Set(queue)];
+    }
+
+    testing() {
+        const test = (listLength) => {
+            const iterations = 10000;
+
+            const comparingFunctions = [
+                this.drain.bind(this),
+                this.drainSet.bind(this),
+                this.drainObject.bind(this),
+                this.drainIndexOf.bind(this),
+                this.drainManual.bind(this),
+                this.drainSmall.bind(this)
+            ];
+
+            console.log(`\nQueue length: ${listLength}`);
+
+            for (const func of comparingFunctions) {
+                let totalTime = 0;
+
+                for (let iteration = 0; iteration < iterations; iteration++) {
+                    this.queue = [];
+
+                    for (let i = 0; i < listLength; i++) {
+                        this.queue.push(
+                            Math.floor(Math.random() * 11) + 1
+                        );
+                    }
+
+                    const timeBefore = performance.now();
+
+                    func();
+
+                    const timeAfter = performance.now();
+
+                    totalTime += timeAfter - timeBefore;
+                }
+
+                const averageTime = totalTime / iterations;
+
+                console.log(
+                    `${func.name}: ${averageTime.toFixed(6)} ms`
+                );
+            }
+        };
+
+        test(0);
+        test(1);
+        test(2);
+        test(10);
+        test(20);
+        test(100);
+        test(1000);
+    }
+}
+
+class Bus {
+    on() {}
+}
+
+const bus = new Bus();
+const eventBuffer = new EventBuffer(bus, "test");
+
+eventBuffer.testing();
 
 
