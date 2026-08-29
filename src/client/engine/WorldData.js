fa78b57fa761a4a738b786ea787841b73a748a3d
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-08-28 23:08:21 -0500

    Update WorldData.js

--- src/client/engine/WorldData.js
+++ src/client/engine/WorldData.js
@@ -25,10 +25,25 @@ export class WorldData {
     getSnapshot() {
         return structuredClone(this.data);
     }
+    
+    // "give entity 0 a location compoment vector3 xyz" 
+    // controller component 
+    // plane component
+    // boatComponent ect 
+    // the boat class knows what composnnts to give to the enrtiy amd sends them here in a stream 
+    addNewComponent(component ) {
+    
+    } 
+        
 
     getIdsByIndex(snapshot, index) {
         return Object.entries(snapshot)
             .filter(([id, entity]) => controllers.includes(entity.controller))
             .map(([id]) => Number(id))
     }
+    applyChanges() {
+
+        
+    } 
+    
 } 
