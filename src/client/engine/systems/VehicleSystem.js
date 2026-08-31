1725a0e46e24c2940173e04b33fd88bebd8a2619
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-08-31 01:06:34 -0500

    Create VehicleSystem.js

---
+++ src/client/engine/systems/VehicleSystem.js
@@ -0,0 +1,34 @@
+
+
+
+class VehicleComponent {
+
+    add()
+
+}
+
+
+class VehicleSystem {
+    comstructor(VehicleComponent,) {
+        
+    
+    }
+    
+
+    start(snapshotData) {
+        for (const entity of snapshotData){
+            
+
+            
+        }
+            
+    } 
+    
+    update() {
+        
+    } 
+    
+    createNew(location) {
+            new this.VehicleComponent(location) 
+    }
+}
