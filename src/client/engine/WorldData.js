5c5203687dce38649cff9347023c0ec29aef0f38
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-09-01 22:55:51 -0500

    Update WorldData.js

--- src/client/engine/WorldData.js
+++ src/client/engine/WorldData.js
@@ -1,6 +1,7 @@
 
 
 
+
 export class WorldData {
     constructor(initalData) {
         this.data = {
@@ -47,3 +48,65 @@ export class WorldData {
     } 
     
 } 
+
+class World {
+    constructor() {
+        this.positions = new Map();
+        this.rotations = new Map();
+        this.controllers = new Map();
+    }
+}
+
+
+
+world.positions.set(42, new Position());
+world.rotations.set(42, new Rotation());
+world.controllers.set(42, new Controller("ai"));
+
+
+class World {
+    constructor() {
+        this.nextEntityId = 0;
+
+        this.position = new Map();
+        this.rotation = new Map();
+        this.velocity = new Map();
+        this.controller = new Map();
+        this.health = new Map();
+    }
+
+    createEntity() {
+        return this.nextEntityId++;
+    }
+}
+const plane = world.createEntity();
+
+world.position.set(plane, new Position(100, 20, 50));
+world.rotation.set(plane, new Rotation());
+world.velocity.set(plane, new Velocity());
+world.controller.set(plane, new Controller("ai"));
+
+
+world.controller.set(42, new Controller("player", playerId));
+
+for (const [entity, position] of world.position) {
+    const velocity = world.velocity.get(entity);
+
+    if (!velocity) continue;
+
+    position.x += velocity.x;
+    position.y += velocity.y;
+    position.z += velocity.z;
+}
+
+
+
+
+
+world.hasVelocity = new Set();
+
+world.hasVelocity.add(42);
+world.hasVelocity.add(57);
+world.hasVelocity.has(42); // true
+
+
